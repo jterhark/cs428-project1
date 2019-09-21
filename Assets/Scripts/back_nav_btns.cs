1e9189ec40cf8b45f753b0c83fb6d40e0be08ab1
jaket <jterha2@uic.edu>
2019-09-21 04:57:22 -0500

    backup

---
+++ Assets/Scripts/back_nav_btns.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class back_nav_btns : MonoBehaviour
+{
+
+    public GameObject videoBtn, infoBtn, reviewsBtn;
+    public GameObject videoPanel, infoPanel, reviewPanels;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+}
